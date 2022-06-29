857f853f14788b414317e30854930ea78b5cc8bf
Tawan 'catflyflyfly' Wattana <c.catfly@gmail.com>
2022-06-30 01:59:31 +0700

    fix: enum to be SCREAMING_SNAKE_CASE

--- src/context.ts
+++ src/context.ts
@@ -7,9 +7,9 @@ export interface Context {
 export const context = {
   taskLists: [
     new TaskList(1, 'Brikllo', [
-      new Task(1, 'Create GraqhQL Server', TaskStatus.Done),
-      new Task(2, 'Design mock APIs', TaskStatus.Done),
-      new Task(3, 'Connect to DB', TaskStatus.InProgress),
+      new Task(1, 'Create GraqhQL Server', TaskStatus.DONE),
+      new Task(2, 'Design mock APIs', TaskStatus.DONE),
+      new Task(3, 'Connect to DB', TaskStatus.IN_PROGRESS),
     ]),
   ],
 };
--- src/models.ts
+++ src/models.ts
@@ -36,6 +36,6 @@ export class Task {
 }
 
 export enum TaskStatus {
-  InProgress,
-  Done,
+  IN_PROGRESS,
+  DONE,
 }
