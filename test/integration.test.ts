b9c2a325a319cc0f5e18f48a200bdbe8da9ace61
Tawan 'catflyflyfly' Wattana <c.catfly@gmail.com>
2022-07-01 13:10:32 +0700

    test: add integration test for mutation apis

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -92,7 +92,7 @@ describe('Brikllo Test Scenarios', () => {
                     },
                     {
                       "id": "10",
-                      "title": "Clean my room",
+                      "title": "Do the dishes",
                       "status": "DONE"
                     }
                   ]
@@ -326,6 +326,370 @@ describe('Brikllo Test Scenarios', () => {
                 {
                   "id": "8",
                   "title": "Pin landmarks in the map"
+                }
+              ]
+            }
+          }
+        `,
+      });
+    });
+  });
+
+  describe('Jamie - manage a new project for Katie', () => {
+    it('initiate new task list', async () => {
+      await testSchema({
+        schema,
+        query: `
+          mutation CreateTaskList($input: TaskListCreateInput!) {
+            createTaskList(input: $input) {
+              id
+              title
+              tasks(input: {}) {
+                id
+                title
+                status
+              }
+            }
+          }
+        `,
+        variables: {
+          input: {
+            title: 'Amazing',
+          },
+        },
+        expectedResponse: `
+          {
+            "data": {
+              "createTaskList": {
+                "id": "4",
+                "title": "Amazing",
+                "tasks": []
+              }
+            }
+          }
+        `,
+      });
+    });
+    it('add (prepend) 5 tasks to the list', async () => {
+      const query = `
+        mutation CreateTask($input: TaskCreateInput!) {
+          createTask(input: $input) {
+            id
+            title
+          }
+        }
+      `;
+      const taskListId = 4;
+
+      await testSchema({
+        schema,
+        query,
+        variables: {
+          input: {
+            taskListId,
+            title: 'Gather resources',
+          },
+        },
+        expectedResponse: `
+          {
+            "data": {
+              "createTask": {
+                "id": "21",
+                "title": "Gather resources"
+              }
+            }
+          }
+        `,
+      });
+      await testSchema({
+        schema,
+        query,
+        variables: {
+          input: {
+            taskListId,
+            title: 'Defend yourselg',
+          },
+        },
+        expectedResponse: `
+          {
+            "data": {
+              "createTask": {
+                "id": "22",
+                "title": "Defend yourselg"
+              }
+            }
+          }
+        `,
+      });
+      await testSchema({
+        schema,
+        query,
+        variables: {
+          input: {
+            taskListId,
+            title: 'Craft weapons',
+          },
+        },
+        expectedResponse: `
+          {
+            "data": {
+              "createTask": {
+                "id": "23",
+                "title": "Craft weapons"
+              }
+            }
+          }
+        `,
+      });
+      await testSchema({
+        schema,
+        query,
+        variables: {
+          input: {
+            taskListId,
+            title: 'Scout the area',
+          },
+        },
+        expectedResponse: `
+          {
+            "data": {
+              "createTask": {
+                "id": "24",
+                "title": "Scout the area"
+              }
+            }
+          }
+        `,
+      });
+      await testSchema({
+        schema,
+        query,
+        variables: {
+          input: {
+            taskListId,
+            title: 'Expand territories',
+          },
+        },
+        expectedResponse: `
+          {
+            "data": {
+              "createTask": {
+                "id": "25",
+                "title": "Expand territories"
+              }
+            }
+          }
+        `,
+      });
+    });
+    it('edit the second task name', async () => {
+      await testSchema({
+        schema,
+        query: `
+          mutation UpdateTask($input: TaskUpdateInput!) {
+            updateTask(input: $input) {
+              id
+              title
+              status
+            }
+          }
+        `,
+        variables: {
+          input: {
+            id: 22,
+            title: 'Defend yourself',
+          },
+        },
+        expectedResponse: `
+          {
+            "data": {
+              "updateTask": {
+                "id": "22",
+                "title": "Defend yourself",
+                "status": "IN_PROGRESS"
+              }
+            }
+          }
+        `,
+      });
+    });
+    it('mark the first task done', async () => {
+      await testSchema({
+        schema,
+        query: `
+          mutation UpdateTask($input: TaskUpdateInput!) {
+            updateTask(input: $input) {
+              id
+              title
+              status
+            }
+          }
+        `,
+        variables: {
+          input: {
+            id: 21,
+            status: 'DONE',
+          },
+        },
+        expectedResponse: `
+          {
+            "data": {
+              "updateTask": {
+                "id": "21",
+                "title": "Gather resources",
+                "status": "DONE"
+              }
+            }
+          }
+        `,
+      });
+    });
+    it('move the fifth task to be the third instead', async () => {
+      await testSchema({
+        schema,
+        query: `
+          mutation MoveTask($input: TaskMoveInput!) {
+            moveTask(input: $input) {
+              id
+              title
+              status
+            }
+          }
+        `,
+        variables: {
+          input: {
+            taskId: 25,
+            taskListId: 4,
+            position: 2,
+          },
+        },
+        expectedResponse: `
+          {
+            "data": {
+              "moveTask": {
+                "id": "25",
+                "title": "Expand territories",
+                "status": "IN_PROGRESS"
+              }
+            }
+          }
+        `,
+      });
+    });
+    it('show the entire board to Katie, showing only in progress tasks', async () => {
+      await testSchema({
+        schema,
+        query: `
+          query Query($input: TaskListQueryInput!, $tasksInput2: TaskQueryInput!) {
+            taskLists(input: $input) {
+              id
+              title
+              tasks(input: $tasksInput2) {
+                id
+                title
+                status
+              }
+            }
+          }
+        `,
+        variables: {
+          input: {},
+          tasksInput2: {
+            status: 'IN_PROGRESS',
+          },
+        },
+        expectedResponse: `
+          {
+            "data": {
+              "taskLists": [
+                {
+                  "id": "1",
+                  "title": "Brikllo",
+                  "tasks": [
+                    {
+                      "id": "4",
+                      "title": "Design mutation APIs",
+                      "status": "IN_PROGRESS"
+                    },
+                    {
+                      "id": "5",
+                      "title": "Fine tuning",
+                      "status": "IN_PROGRESS"
+                    }
+                  ]
+                },
+                {
+                  "id": "2",
+                  "title": "Camping vacation preps",
+                  "tasks": [
+                    {
+                      "id": "6",
+                      "title": "Pack my bag",
+                      "status": "IN_PROGRESS"
+                    },
+                    {
+                      "id": "7",
+                      "title": "Prepare food",
+                      "status": "IN_PROGRESS"
+                    }
+                  ]
+                },
+                {
+                  "id": "3",
+                  "title": "Finish that book",
+                  "tasks": [
+                    {
+                      "id": "11",
+                      "title": "Chapter 1",
+                      "status": "IN_PROGRESS"
+                    },
+                    {
+                      "id": "12",
+                      "title": "Chapter 2",
+                      "status": "IN_PROGRESS"
+                    },
+                    {
+                      "id": "13",
+                      "title": "Chapter 3",
+                      "status": "IN_PROGRESS"
+                    },
+                    {
+                      "id": "14",
+                      "title": "Chapter 4",
+                      "status": "IN_PROGRESS"
+                    },
+                    {
+                      "id": "15",
+                      "title": "Chapter 5",
+                      "status": "IN_PROGRESS"
+                    }
+                  ]
+                },
+                {
+                  "id": "4",
+                  "title": "Amazing",
+                  "tasks": [
+                    {
+                      "id": "24",
+                      "title": "Scout the area",
+                      "status": "IN_PROGRESS"
+                    },
+                    {
+                      "id": "25",
+                      "title": "Expand territories",
+                      "status": "IN_PROGRESS"
+                    },
+                    {
+                      "id": "23",
+                      "title": "Craft weapons",
+                      "status": "IN_PROGRESS"
+                    },
+                    {
+                      "id": "22",
+                      "title": "Defend yourself",
+                      "status": "IN_PROGRESS"
+                    }
+                  ]
                 }
               ]
             }
