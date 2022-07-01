6e4b011ef3dd1de79b12fceeab015e1b36b48c57
Tawan 'catflyflyfly' Wattana <c.catfly@gmail.com>
2022-07-01 11:26:43 +0700

    feat: handle rank rebalancing in case new top rank cannot be found

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,4 @@
+import R from 'ramda';
 import { Context } from '../context';
 import { TaskList, TaskStatus } from '../graphql/models';
 import {
@@ -7,6 +8,7 @@ import {
   TaskQueryInput,
 } from '../graphql/inputs';
 import rankService from './rank';
+import * as db from '@prisma/client';
 
 const getTaskLists = async (input: TaskListQueryInput, ctx: Context) => {
   return ctx.prisma.taskList.findMany({
@@ -70,19 +72,60 @@ const createTask = async (input: TaskCreateInput, ctx: Context) => {
 
   const newTopRank = rankService.getNewTopRank(taskListTopRank);
 
-  // TODO: handle case newTopRank is undefined, which means topRank is at limit
-  //          - findAll -> rebalanceAll -> upsertAll
+  if (newTopRank === undefined) {
+    const existingTasks = await ctx.prisma.task.findMany({
+      where: { taskListId: input.taskListId },
+      orderBy: { rank: 'desc' },
+    });
 
-  return ctx.prisma.task.create({
-    data: {
-      createdBy: 'hello',
-      updatedBy: 'hello',
-      title: input.title,
-      status: TaskStatus.IN_PROGRESS,
-      taskListId: input.taskListId,
-      rank: newTopRank!,
-    },
+    const newTask = await ctx.prisma.task.create({
+      data: {
+        createdBy: 'hello',
+        updatedBy: 'hello',
+        title: input.title,
+        status: TaskStatus.IN_PROGRESS,
+        taskListId: input.taskListId,
+        rank: rankService.MAX,
+      },
+    });
+
+    const allTasks = [newTask, ...existingTasks];
+
+    return (await upsertRebalanceTasks(allTasks, ctx))[0];
+  } else {
+    return ctx.prisma.task.create({
+      data: {
+        createdBy: 'hello',
+        updatedBy: 'hello',
+        title: input.title,
+        status: TaskStatus.IN_PROGRESS,
+        taskListId: input.taskListId,
+        rank: newTopRank,
+      },
+    });
+  }
+};
+
+const upsertRebalanceTasks = async (orderedTasks: db.Task[], ctx: Context) => {
+  const newRanks = rankService.balancedRanks(orderedTasks.length);
+
+  R.zip(orderedTasks, newRanks).forEach(([task, newRank]) => {
+    task.rank = newRank;
   });
+
+  return Promise.all(
+    orderedTasks.map((task) => {
+      return ctx.prisma.task.upsert({
+        where: { id: task.id },
+        update: {
+          ...task,
+        },
+        create: {
+          ...task,
+        },
+      });
+    }),
+  );
 };
 
 const graphqlService = {
